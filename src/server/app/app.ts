3292103c27d3c09fa99d96aa9b600288cd1be116
Pedr <pedr.browne@gmail.com>
2026-02-23 09:14:58 +0000

    Add basic error handling to api route

--- src/server/app/app.ts
+++ src/server/app/app.ts
@@ -1,16 +1,20 @@
-import express from 'express'
+import express, { type Request } from 'express'
 import { LOG_LEVEL } from '../const/LOG_LEVEL'
 import { db } from '../db'
 import { logEntries } from '../db/schema'
 
 export const app = express()
 
-app.get(`/ping`, (_req, res) => {
-  db.insert(logEntries)
-    .values({
-      message: `Ping`,
-      level: LOG_LEVEL.INFO,
-    })
-    .run()
-  res.json({ success: true })
+app.get(`/ping`, (req: Request<{ id: string }>, res) => {
+  try {
+    db.insert(logEntries)
+      .values({
+        message: `Ping`,
+        level: LOG_LEVEL.INFO,
+      })
+      .run()
+    res.json({ success: true })
+  } catch {
+    res.status(500).json({ success: false, error: `Internal server error` })
+  }
 })
