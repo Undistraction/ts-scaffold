783cc6e88b34c9d45605c3317eadd9beeca4588b
Pedr <pedr.browne@gmail.com>
2026-03-09 11:56:09 +0000

    chore: Add commit-lint w/ conventional commits

---
+++ commitlint.config.ts
@@ -0,0 +1,38 @@
+// CommitLint Configuration
+const config = {
+  // config-conventional is based on Conventional Commit:
+  // https://conventionalcommits.org/
+  extends: [`@commitlint/config-conventional`],
+  rules: {
+    'type-enum': [
+      2,
+      `always`,
+      [
+        `build`,
+        `ci`,
+        `chore`,
+        `content`,
+        `debug`,
+        `deps`,
+        `docs`,
+        `feat`,
+        `fix`,
+        `hotfix`,
+        `merge`,
+        `perf`,
+        `refactor`,
+        `revert`,
+        `style`,
+        `test`,
+      ],
+    ],
+    // Scope can be any case.
+    'scope-case': [0, `always`],
+    // Subject should be sentence case.
+    'subject-case': [2, `always`, `sentence-case`],
+    'body-max-line-length': [0, `always`, `Infinity`],
+    'footer-max-line-length': [0, `always`, `Infinity`],
+  },
+}
+
+export default config
