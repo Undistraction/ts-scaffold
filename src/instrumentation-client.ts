b1ca584de0532d39f5fe0765f153e188c0f84e5c
Pedr <pedr.browne@gmail.com>
2026-03-10 20:31:49 +0000

    chore: Tweak sentry config to explictly disable outside production env

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,8 +2,8 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.NODE_ENV === `production`,
   integrations: [Sentry.replayIntegration()],
-  tracesSampleRate: process.env.NODE_ENV === `development` ? 1.0 : 0.1,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
   enableLogs: true,
